1480d1c3cba2286b1f47998f27206ba4e3420b5e
unknown <extapo@yandex>
2025-06-25 16:13:08 +0700

    mailing

---
+++ mailing/management/commands/sent_letter.py
@@ -0,0 +1,43 @@
+from django.core.mail import send_mail
+from django.core.management import BaseCommand
+from django.utils import timezone
+
+from config.settings import EMAIL_HOST_USER
+from mailing.models import AttemptMailing, Mailing
+
+
+class Command(BaseCommand):
+    help = "Отправка рассылки"
+
+    def handle(self, *args, **kwargs):
+        mailings = Mailing.objects.filter(
+            status__in=[Mailing.CREATED, Mailing.LAUNCHED]
+        )
+        for mailing in mailings:
+            for recipient in mailing.recipients.all():
+                try:
+                    send_mail(
+                        mailing.message.subject,
+                        mailing.message.content,
+                        from_email=EMAIL_HOST_USER,
+                        recipient_list=[recipient.email],
+                        fail_silently=False,
+                    )
+                    AttemptMailing.objects.create(
+                        date_attempt=timezone.now(),
+                        status=AttemptMailing.STATUS_OK,
+                        server_response="Email отправлен",
+                        mailing=mailing,
+                    )
+                    print(
+                        f"Сообщение {mailing.message.subject} успешно отправлено получателю: {recipient.email}"
+                    )
+                except Exception as e:
+                    AttemptMailing.objects.create(
+                        date_attempt=timezone.now(),
+                        status=AttemptMailing.STATUS_NOK,
+                        server_response=str(e),
+                        mailing=mailing,
+                    )
+                    print(str(e))
+            mailing.save()
